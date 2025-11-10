4064780d5589c5e5c3e644c7b66ea7613386ffd0
Shrikant Soni <shrikantsoni809@gmail.com>
2025-11-10 16:02:26 +0530

    removed unused variables

--- frontend/src/page/Profile.tsx
+++ frontend/src/page/Profile.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Navbar } from "@/components/Navbar";
 import UserProfileCard from "@/components/UserProfileCard";
 import UserBlogs from "@/components/UserBlogs";
